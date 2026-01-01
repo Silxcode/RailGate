2ec5e78c3eeb3adb230f3759ee015620e1f30592
Sylvester <sylvesterfernandes.939@gmail.com>
2026-01-01 14:35:59 +0530

    Feat: Create Admin Dashboard UI and updated Vite config for multi-page app

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,8 @@ export default defineConfig({
 
         rollupOptions: {
             input: {
-                main: './index.html'
+                main: resolve(__dirname, 'index.html'),
+                admin: resolve(__dirname, 'admin.html')
             }
         }
     }
